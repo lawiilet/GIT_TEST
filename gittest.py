b5492f4cd1028e1b51291b3a52489beedc7a872f
lawiilet <huangd666hd@gmail.com>
2020-03-16 20:40:00 +0800

    新增了一行注释代码

--- gittest.py
+++ gittest.py
@@ -13,4 +13,4 @@ print(dt.strftime('%y%m%d %H:%M'))
 print(dt.strptime('20200316 20:17:01', '%Y%m%d %H:%M:%S'))
 print(type(dt.strptime('20200316', '%Y%m%d')))
 
-# 111111111111111111111111111
+# 1111111111111111111111
